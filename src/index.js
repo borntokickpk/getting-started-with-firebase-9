8e5df27027da78a157146cc0e14892d665fd41df
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2021-11-25 22:13:14 +0500

    firebase  8 way of importing firebase

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkP9ksYPulu7jCp8nDqa4jsRfiZ7EitnE",
+    authDomain: "getting-started-with-fir-30780.firebaseapp.com",
+    projectId: "getting-started-with-fir-30780",
+    storageBucket: "getting-started-with-fir-30780.appspot.com",
+    messagingSenderId: "353703001954",
+    appId: "1:353703001954:web:3811c83d266a3dac2eb63b",
+};
+
+firebase.initializeApp(firebaseConfig);
