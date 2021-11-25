ce4203b5b1c790c53a19ddacbc34f342096f6a0b
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2021-11-25 22:11:00 +0500

    connected bunlde.js with html

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const firebaseConfig = {\n    apiKey: \"AIzaSyDkP9ksYPulu7jCp8nDqa4jsRfiZ7EitnE\",\n    authDomain: \"getting-started-with-fir-30780.firebaseapp.com\",\n    projectId: \"getting-started-with-fir-30780\",\n    storageBucket: \"getting-started-with-fir-30780.appspot.com\",\n    messagingSenderId: \"353703001954\",\n    appId: \"1:353703001954:web:3811c83d266a3dac2eb63b\",\n};\n\n\n//# sourceURL=webpack://getting-started-with-firebase-9/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
