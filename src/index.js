2ca652c99ea7b627830451479b4ffff6ef451d5f
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2021-11-25 22:30:35 +0500

    Got data from firebase

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDkP9ksYPulu7jCp8nDqa4jsRfiZ7EitnE",
@@ -9,4 +10,21 @@ const firebaseConfig = {
     appId: "1:353703001954:web:3811c83d266a3dac2eb63b",
 };
 
+// init firebase app
 initializeApp(firebaseConfig);
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "books");
+
+// get collection data
+getDocs(colRef)
+    .then((snapshot) => {
+        let books = [];
+        snapshot.docs.forEach((doc) => {
+            books.push({ id: doc.id, ...doc.data() });
+        });
+        console.log(books);
+    })
+    .catch((err) => {});
